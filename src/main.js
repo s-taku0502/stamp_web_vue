7854e8106e955cdf962d4e2d654dfdf5445c54fc
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-06 04:48:11 +0900

    fix:cssをインポート

--- src/main.js
+++ src/main.js
@@ -5,10 +5,9 @@ import './assets/css/home.css';
 import './assets/css/template.css';
 import './assets/css/scan.css';
 
-/*
 import './assets/css/stamps.css';
 import './assets/css/coupons.css';
-import './assets/css/stores.css'; */
+import './assets/css/stores.css';
 import './assets/css/contact.css';
 
 createApp(App).use(router).mount('#app');
