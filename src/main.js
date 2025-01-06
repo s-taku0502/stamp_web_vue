a3ddbfc6e47cdd1b4bbd13bb9c783617643d8c1a
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-06 16:04:30 +0900

    del:css部分をコメントアウト

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from 'vue';
 import App from '../App.vue';
 import router from './router';
-import './assets/css/home.css';
-import './assets/css/template.css';
-import './assets/css/scan.css';
+/*
+import '@/assets/css/reset.css';
+import '@/assets/css/home.css';
+import '@/assets/css/template.css';
+import '@/assets/css/scan.css';
 
 import './assets/css/stamps.css';
 import './assets/css/coupons.css';
 import './assets/css/stores.css';
 import './assets/css/contact.css';
+*/
 
 createApp(App).use(router).mount('#app');
