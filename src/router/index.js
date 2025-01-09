4df7ef052ff85d29a1038e1cdb027764cf802606
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-10 03:09:13 +0900

    update:ルーターの設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import { QrcodeStream } from 'vue-qrcode-reader'
+import { QrcodeStream } from 'vue-qrcode-reader';
 
 import RegisterLayout from '@/layouts/RegisterLayout.vue';
 import Login from '@/views/Login.vue';
 import Home from '@/views/Home.vue';
 import Scan from '@/views/Scan.vue';
-import Stamps from '@/views/Stamps.vue';
 import Coupons from '@/views/Coupons.vue';
 import Stores from '@/views/Stores.vue';
 import Contact from '@/views/Contact.vue';
 import Register from '@/views/Register.vue';
 import NotFound from '@/views/NotFound.vue';
+import AllStamps from '@/views/AllStamps.vue';
+import CurrentStamps from '@/views/CurrentStamps.vue';
+import UserProfile from '@/views/UserProfile.vue';
 
 const routes = [
   { path: '/', name: 'ログイン画面', component: Login },
@@ -19,10 +21,12 @@ const routes = [
     name: 'スキャン画面', 
     component: Scan,
     props: (route) => ({ onScanComplete: route.query.onScanComplete }) },
-  { path: '/stamps', name: 'スタンプ画面', component: Stamps },
+  { path: '/current-stamps', name: '開催中のスタンプ', component: CurrentStamps },
+  { path: '/all-stamps', name: '過去のスタンプ', component: AllStamps },
   { path: '/coupons', name: 'クーポン画面', component: Coupons },
   { path: '/stores', name: '店舗一覧', component: Stores },
   { path: '/contact', name: 'お問い合わせ', component: Contact },
+  { path: '/user-profile', name: 'マイページ', component: UserProfile },
   {
     path: '/register',
     name: '新規登録',
@@ -35,11 +39,11 @@ const routes = [
     component: Login,
     meta: { layout: RegisterLayout },
   },
-  { path: '/:pathMatch(.*)*', name: '404NotFound', component: NotFound },
+  { path: '/:catchAll(.*)', name: '404NotFound', component: NotFound },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
