dc2d3ab74ae1d23ea20cf8aaeb42bc1e3d59d24f
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-10 02:50:25 +0900

    fix:Firebaseの再接続

--- src/main.js
+++ src/main.js
@@ -2,7 +2,29 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import RegisterLayout from './layouts/RegisterLayout.vue';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
+// Firebaseの設定 (Firebase Console からコピー)
+const firebaseConfig = {
+    apiKey: "AIzaSyCX-MwBvHlOe8hKVWFq4CrisV0MKUHwJfY",
+    authDomain: "kanazawa-nuka2024.firebaseapp.com",
+    projectId: "kanazawa-nuka2024",
+    storageBucket: "kanazawa-nuka2024.firebasestorage.app",
+    messagingSenderId: "729877460278",
+    appId: "1:729877460278:web:3e388b308f1d6ad0192f97",
+    measurementId: "G-WZ2GHT12EV"
+};
+
+// Firebaseの初期化
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+// Vueアプリケーションの初期化
 const app = createApp(App);
 
 // グローバルコンポーネントの登録
