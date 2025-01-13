65c5f186e8a1eaf625311f4302b139c879341331
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-13 15:36:12 +0900

    fix:重複部分を削除

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,6 @@
 import { getAuth } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 
-export async function checkAuthAndRedirect(router) {
-  const auth = getAuth();
-  const user = auth.currentUser;
-  if (!user) {
-    router.push('/login'); // ユーザーが認証されていない場合、ログインページにリダイレクト
-  }
-}
-
 export async function checkAdminAndRedirect(router) {
   const auth = getAuth();
   const user = auth.currentUser;
