3b0e13affc22fc788c0ebaaa6840c3bd19747f93
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-16 01:01:54 +0900

    add:お知らせ欄の作成

---
+++ src/views/News.vue
@@ -0,0 +1,40 @@
+<template>
+    <main class="info-box">
+      <h3 class="title">お知らせ</h3>
+      <p class="content">最新の情報をお届けします。</p>
+    </main>
+  </template>
+  
+  <script>
+  export default {
+    name: "LatestInfo",
+  };
+  </script>
+  
+  <style scoped>
+  .info-box {
+    align-items: center;
+    border: 1px solid #444;
+    width: 90%;
+    height: auto;
+    margin: 0 auto;
+    margin-bottom: 30px;
+    position: relative;
+  }
+  
+  .title {
+    position: absolute;
+    top: -12px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #fff;
+    padding: 0 10px;
+    font-weight: bold;
+  }
+  
+  .content {
+    margin: 50px 20px;
+    text-align: center;
+  }
+  </style>
+  
