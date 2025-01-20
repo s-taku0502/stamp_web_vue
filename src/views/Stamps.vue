9e8b8b447d8120d8f845a4061e057e3a01dcf1d8
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-20 17:51:56 +0900

    fix:画像を縮小

--- src/views/Stamps.vue
+++ src/views/Stamps.vue
@@ -1,62 +1,62 @@
 <template>
-  <div id="Stamps">
-    <div class="tabs">
-      <button @click="currentTab = 'current'">Current Stamps</button>
-      <button @click="currentTab = 'all'">All Stamps</button>
+    <div id="Stamps">
+      <div class="tabs">
+        <button @click="currentTab = 'current'">Current Stamps</button>
+        <button @click="currentTab = 'all'">All Stamps</button>
+      </div>
+      <div class="tab-content">
+        <CurrentStamps v-if="currentTab === 'current'" :imageUrl="imageUrl" />
+        <AllStamps v-if="currentTab === 'all'" />
+      </div>
     </div>
-    <div class="tab-content">
-      <CurrentStamps v-if="currentTab === 'current'" :imageUrl="imageUrl" />
-      <AllStamps v-if="currentTab === 'all'" />
-    </div>
-  </div>
-</template>
-
-<script>
-import CurrentStamps from '@/views/CurrentStamps.vue';
-import AllStamps from '@/views/AllStamps.vue';
-
-export default {
-  components: {
-    CurrentStamps,
-    AllStamps
-  },
-  data() {
-    return {
-      currentTab: 'current',
-      imageUrl: '' // 初期値を設定
-    };
-  },
-  methods: {
-    setImageUrl(url) {
-      this.imageUrl = url;
+  </template>
+  
+  <script>
+  import CurrentStamps from '@/views/CurrentStamps.vue';
+  import AllStamps from '@/views/AllStamps.vue';
+  
+  export default {
+    components: {
+      CurrentStamps,
+      AllStamps
+    },
+    data() {
+      return {
+        currentTab: 'current',
+        imageUrl: '' // 初期値を設定
+      };
+    },
+    methods: {
+      setImageUrl(url) {
+        this.imageUrl = url;
+      }
     }
+  };
+  </script>
+  
+  <style scoped>
+  #Stamps {
+    text-align: center;
   }
-};
-</script>
-
-<style scoped>
-#Stamps {
-  text-align: center;
-}
-
-.tabs {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 20px;
-}
-
-.tabs button {
-  padding: 10px 20px;
-  margin: 0 5px;
-}
-
-.tabs button.active {
-  background-color: #4caf50;
-  color: white;
-}
-
-.tab-content {
-  display: flex;
-  justify-content: center;
-}
-</style>
+  
+  .tabs {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+  
+  .tabs button {
+    padding: 10px 20px;
+    margin: 0 5px;
+  }
+  
+  .tabs button.active {
+    background-color: #4caf50;
+    color: white;
+  }
+  
+  .tab-content {
+    display: flex;
+    justify-content: center;
+  }
+  </style>
