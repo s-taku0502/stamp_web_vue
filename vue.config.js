14bff2c5082ad952c72ff5e39652c62c6bb043a8
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-01-23 19:32:46 +0900

    add:firebase連携に必要なwebpackを追加

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
+const webpack = require('webpack');
+
 module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
   devServer: {
     historyApiFallback: true
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        '__VUE_OPTIONS_API__': JSON.stringify(true),
+        '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
+      })
+    ]
   }
 };
